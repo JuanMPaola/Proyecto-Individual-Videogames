28750f0647d338013c2d5a3a3ac275e64321a0a2
JuanMPaola <juanmpaola25@gmail.com>
2023-09-25 09:49:36 -0300

    Empezando proyecto

---
+++ api/src/models/Videogame.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('videogame', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    plataforms: {
+      type: DataTypes.INTEGER,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    realeseData: {
+      type: DataTypes.DATE,
+    },
+    rating: {
+      type: DataTypes.INTEGER
+    }
+  },{timestamps: false});
+};
