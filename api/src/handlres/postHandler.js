2e7581cf03cfb133749ca10f76eb3f47e4507582
JuanMPaola <juanmpaola25@gmail.com>
2023-10-03 22:57:26 -0300

    Base de datos Genres ahora funcionan

--- api/src/handlres/postHandler.js
+++ api/src/handlres/postHandler.js
@@ -2,7 +2,6 @@ const { Videogame, Genre } = require('../db');
 
 const postHandler = async ({ name, background_image, description, platforms, realesed, genres, rating }) => {
     try {
-
         // Verificar si el videojuego ya existe en la base de datos
         const [videogame, created] = await Videogame.findOrCreate({
             where: {
@@ -12,34 +11,35 @@ const postHandler = async ({ name, background_image, description, platforms, rea
                 background_image,
                 rating,
                 platforms,
-                
             },
         });
 
-        console.log(genres)
+        console.log(genres);
 
         // Asociar los géneros con el videojuego
         if (genres) {
-            for (const genreName of genres) {
+            /* for (const genreName of genres) { */
                 const genre = await Genre.findOne({
-                    where: { name: genreName },
+                    where: { name: [genres] },
                 });
                 if (genre) {
-                    await videogame.addGenre(genre.id);
-                    console.log(`Género "${genreName}" asociado.`);
+                    await videogame.addGenre(genre); 
+                   /*  console.log(`Género "${genreName}" asociado.`); */
                 }
-            }
-            const game = await Videogame.findOne({
-                where:{id: videogame.id},
-                include:[
+            /* } */
+
+            // Verificar si los géneros se asociaron correctamente
+            const gameWithGenres = await Videogame.findOne({
+                where: { id: videogame.id },
+                include: [
                     {
                         model: Genre,
                         attributes: ["name"],
-                        through: { attributes:[]}
+                        through: { attributes: [] }
                     }
                 ]
-            })
-            return game;
+            });
+            console.log("Videojuego con géneros asociados:", gameWithGenres.toJSON());
         }
 
         return videogame; // Importante: devolver el videojuego creado
